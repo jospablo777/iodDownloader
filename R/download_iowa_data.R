04bad4c494a447f9c32c1c88995ff459df1f09fe
Jose P <jospablo777@gmail.com>
2024-02-03 00:17:59 -0600

    Update functions examples

--- R/download_iowa_data.R
+++ R/download_iowa_data.R
@@ -3,7 +3,7 @@
 
 #' Data set batch downloader
 #'
-#' `download_iowa_data()` downloads specified data from the Iowa Open Data portal (https://data.iowa.gov/).
+#' @description `download_iowa_data()` downloads specified data from the Iowa Open Data portal (https://data.iowa.gov/).
 #'
 #' @param data_id A string. You get it from the data set page in. This can be found on the url of your data set of interest; for example the url for Iowa Fire Department Census data set is "https://data.iowa.gov/Emergency-Management/Iowa-Fire-Department-Census/hv43-6ksq/about_data" here the id is the value nex to the data set name on the right (i.e., "hv43-6ksq").
 #' @param folder A string. The path to the folder you will save the data.
@@ -15,8 +15,16 @@
 #' @export
 #'
 #' @examples
-#' # Here we download the Iowa Fire Department Census data set. If you go to the dataset web page (https://data.iowa.gov/Emergency-Management/Iowa-Fire-Department-Census/hv43-6ksq/about_data), you will find that the total number of rows of this data set (to the date, 26-Jan-2024) is 738, so if we want the whole data set it is okay if we set total_of_rows=1000, and batch_size=1000.
-#' download_iowa_data(data_id='hv43-6ksq', folder='data', data_name='fire_department_census', total_of_rows=1000, batch_size=1000)
+#' Here we download the Iowa Fire Department Census data set.
+#' If you go to the dataset web page
+#' (https://data.iowa.gov/Emergency-Management/Iowa-Fire-Department-Census/hv43-6ksq/about_data),
+#' you will find that the total number of rows of this data set (to the date, 26-Jan-2024) is 738, so if we want the whole data
+#' set it is okay if we set `total_of_rows = 1000`, and `batch_size = 1000`.
+#' download_iowa_data(data_id   = 'hv43-6ksq',
+#'                    folder    = 'data',
+#'                    data_name = 'fire_department_census',
+#'                    total_of_rows = 1000,
+#'                    batch_size    = 1000)
 download_iowa_data <- function(data_id, folder, data_name, total_of_rows=10000, batch_size=5000) {
   options(readr.show_col_types = FALSE, # Hide readr messages
           readr.show_progress = FALSE)
@@ -25,11 +33,11 @@ download_iowa_data <- function(data_id, folder, data_name, total_of_rows=10000, 
   n_iterations   <- ceiling(total_of_rows/batch_size)
 
   # Add a progress bar to track the advance in the data pulling.
-  pb <- txtProgressBar(min = 0,
-                       max = n_iterations,
-                       style = 3,
-                       width = 50,
-                       char = "=")
+  pb <- utils::txtProgressBar(min = 0,
+                              max = n_iterations,
+                              style = 3,
+                              width = 50,
+                              char = "=")
 
   # we will pull our data in batches of 1 million of rows
   offset <- 0
@@ -61,19 +69,19 @@ download_iowa_data <- function(data_id, folder, data_name, total_of_rows=10000, 
     # But first we must check if the directory for our data exist
     if (file.exists(file_final_folder)){
 
-      write_csv(df, file_name) # just saves the file if directory exists
+      readr::write_csv(df, file_name) # just saves the file if directory exists
     } else {
       # Creates the directory
       dir.create(file.path(file_final_folder))
 
       # Saves the file
-      write_csv(df, file_name)
+      readr::write_csv(df, file_name)
     }
 
     # Update offset for next batch
     offset <- offset + batch_size
 
     # Progress bar update
-    setTxtProgressBar(pb, i)
+    utils::setTxtProgressBar(pb, i)
   }
 }
--- R/read_iowa_data.R
+++ R/read_iowa_data.R
@@ -3,7 +3,7 @@
 
 #' Read the data pulled from Iowa Open Data portal (https://data.iowa.gov/).
 #'
-#' `read_iowa_data` reads the data pulled from the Iowa Open Data portal API with the function `download_iowa_data`
+#' @description  `read_iowa_data` reads the data pulled from the Iowa Open Data portal API with the function `download_iowa_data`
 #'
 #' @param folder_path A string. The path to the folder you saved your data.
 #' @param data_name A string. How your data set common files are called in your file system.
@@ -12,8 +12,10 @@
 #' @export
 #'
 #' @examples
-#' # Read the fire_department_census data you downloaded with th download_iowa_data() function. The folder 'data' with the data is located in your R project working directory.
-#' read_iowa_data(folder_path='data', data_name='fire_department_census')
+#' Read the fire_department_census data you downloaded with the download_iowa_data() function.
+#' The folder 'data' with the data is located in your R project working directory.
+#' read_iowa_data(folder_path = 'data',
+#'                data_name   = 'fire_department_census')
 read_iowa_data <- function(folder_path='data', data_name) {
   options(readr.show_col_types = FALSE, # hide readr messages
           readr.show_progress = FALSE)
@@ -24,11 +26,11 @@ read_iowa_data <- function(folder_path='data', data_name) {
                           full.names=TRUE)
 
   # Add a progress bar to track the advance in the data pulling.
-  pb <- txtProgressBar(min = 0,
-                       max = length(filenames),
-                       style = 3,
-                       width = 50,
-                       char = "=")
+  pb <- utils::txtProgressBar(min = 0,
+                              max = length(filenames),
+                              style = 3,
+                              width = 50,
+                              char = "=")
 
   for(i in 1:length(filenames)){
 
@@ -42,7 +44,7 @@ read_iowa_data <- function(folder_path='data', data_name) {
     }
 
     # Update progress bar
-    setTxtProgressBar(pb, i)
+    utils::setTxtProgressBar(pb, i)
   }
 
   # sort data by date
